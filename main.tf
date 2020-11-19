f583a926dd41eec21f702d68b6f1b4a5c61caf02
chris3ware <chris@3ware.co.uk>
2020-11-19 20:19:34 +0000

    added main.tf

---
+++ main.tf
@@ -0,0 +1,137 @@
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_vpc" "target" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_vpc" "agent" {
+  cidr_block = "10.100.100.0/24"
+}
+
+resource "aws_subnet" "targetSubnet" {
+  vpc_id                  = aws_vpc.target.id
+  cidr_block              = "10.0.0.0/26"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "agentSubnet" {
+  vpc_id                  = aws_vpc.agent.id
+  cidr_block              = "10.100.100.0/26"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "targetIGW" {
+  vpc_id = aws_vpc.target.id
+}
+
+resource "aws_internet_gateway" "agentIGW" {
+  vpc_id = aws_vpc.agent.id
+}
+
+resource "aws_route" "targetInetAccess" {
+  route_table_id         = aws_vpc.target.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.targetIGW.id
+}
+
+resource "aws_route" "agentInetAccess" {
+  route_table_id         = aws_vpc.agent.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.agentIGW.id
+}
+
+resource "aws_security_group" "agentEndpointSG" {
+  name   = "agentEndpointSG"
+  vpc_id = aws_vpc.agent.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # In [] because it's a list
+  }
+}
+
+resource "aws_security_group" "targetAmiSG" {
+  name = "targetAmiSG"
+  vpc_id = aws_vpc.target.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["82.68.125.46/32", aws_subnet.agentSubnet.cidr_block]
+  }
+}
+
+resource "aws_security_group" "agentAmiSG" {
+  name = "agentAmiSG"
+  vpc_id = aws_vpc.agent.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["82.68.125.46/32"]
+  }
+}
+
+resource "aws_elb" "privateLinkNLB" {
+  name     = "privateLinkNLB"
+  internal = true
+  subnets  = [aws_subnet.targetSubnet.id]
+
+  listener {
+    instance_port     = 22
+    instance_protocol = "tcp"
+    lb_port           = 22
+    lb_protocol       = "tcp"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:22"
+    interval            = 30
+  }
+  instances = [aws_instance.targetEC2.id]
+}
+
+resource "aws_vpc_endpoint_service" "targetEndpoint" {
+  acceptance_required        = false
+  network_load_balancer_arns = [aws_elb.privateLinkNLB.arn]
+}
+
+resource "aws_vpc_endpoint" "agentEndpoint" {
+  vpc_id             = aws_vpc.agent.id
+  service_name       = aws_vpc_endpoint_service.targetEndpoint.service_name
+  vpc_endpoint_type  = "Interface"
+  security_group_ids = [aws_security_group.agentEndpointSG.id]
+  subnet_ids          = [aws_subnet.agentSubnet.id]
+
+}
+
+resource "aws_key_pair" "chrisKey" {
+  key_name   = "chrisKey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5maRkyxqPSYagtBpg00tNnTvjdYvnKTIworAp0mceVFxefDu/8Zw/dSrznq0Jz1fzbKXSElf05wKEO6TCV6Wy2bMG1BvTgiyDA/BXUwcyKnu3sJwUY2KzRGvvGIFlTboRhI3C+E5qijTwyINfcNUFRvYZtCm6BrfCO5762sJezMLYUIFnQjqNgjBx7ezDP4/u1h7EEBdqUd8MhaYloszQMymfFf2hkLd53yubwOUw0V7VMmZuuBvUmto1sQl7W9+amHCqkoDwN9lOetQxE8gIDZ+FvEe8SPIMolX70XDGvIJL09Ne8Xq85EhBb55BYwM5rIa+ZnVQOTkwyzHcQJ1r chris@3waremac.localnet"
+}
+
+resource "aws_instance" "targetEC2" {
+  ami             = "ami-0271d331ac7dab654"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.targetSubnet.id
+  security_groups = [aws_security_group.targetAmiSG.id]
+  key_name        = aws_key_pair.chrisKey.id
+
+}
+
+resource "aws_instance" "agentEC2" {
+  ami             = "ami-0271d331ac7dab654"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.agentSubnet.id
+  security_groups = [aws_security_group.agentAmiSG.id]
+  key_name        = aws_key_pair.chrisKey.id
+}
