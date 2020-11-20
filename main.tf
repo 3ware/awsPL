3aa36f57eacf0be812f090039ac2a609c8e6438d
chris3ware <chris@3ware.co.uk>
2020-11-20 10:40:18 +0000

    fixed healthcheck protocol HTTP to TCP

--- main.tf
+++ main.tf
@@ -119,16 +119,13 @@ resource "aws_lb_target_group" "targetTG" {
   protocol = "TCP"
   vpc_id   = aws_vpc.target.id
   health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
     port                = 22
-    interval            = 30
+    protocol = "TCP"
   }
 }
 
 resource "aws_lb_listener" "privateLinkNLB" {
-  load_balancer_arn = "aws_lb_privateLinkNLB.arn"
+  load_balancer_arn = aws_lb.privateLinkNLB.arn
   port              = "22"
   protocol          = "TCP"
   default_action {
